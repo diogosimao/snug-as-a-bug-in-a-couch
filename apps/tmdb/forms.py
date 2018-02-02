b453cfac11df75b594b14452084ed4c407700c52
Diogo Simão <diogosimao@gmail.com>
2018-02-02 10:50:00 -0200

    Updated form
    Changed checkbox to select list
    
    Updated views to fill with movie choices after query at tmdb
    
    Updated template

--- apps/tmdb/forms.py
+++ apps/tmdb/forms.py
@@ -2,8 +2,19 @@ from django import forms
 
 
 class QueryForm(forms.Form):
-    CHOICES = [(True, 'Release at 2018')]
 
-    choices = forms.ChoiceField(required=False, choices=CHOICES, widget=forms.RadioSelect())
+    def __init__(self, movies_choices_list=[], *args, **kwargs):
+        super(QueryForm, self).__init__(*args, **kwargs)
+        self.fields['movies_choices'].choices = movies_choices_list
+
+    FILTER_CHOICES = [(True, 'Release at 2018')]
+
+    filters_choices = forms.ChoiceField(required=False, choices=FILTER_CHOICES, widget=forms.RadioSelect())
     search = forms.CharField(required=False, max_length=255)
+
+    movies_choices = forms.MultipleChoiceField(
+        required=False,
+        choices=(),
+        help_text='Check as many as you like.',
+    )
 
--- apps/tmdb/views.py
+++ apps/tmdb/views.py
@@ -1,33 +1,34 @@
 import html
 
-import tmdbsimple as tmdb
+import tmdbsimple
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
 from django.conf import settings
 
 from .forms import QueryForm
 
-tmdb.API_KEY = settings.TMDB_API_KEY
+tmdbsimple.API_KEY = settings.TMDB_API_KEY
 
 
 def query_view(request):
-    search = tmdb.Search()
-
-    form = QueryForm()
-    context = {'form': form}
-
+    movies_choices_list = []
     if request.method == 'POST':
-        form = QueryForm(request.POST)
+        form = QueryForm([], request.POST)
         if form.is_valid():
+            search = tmdbsimple.Search()
             search_string = html.escape(form.data.get('search', ''))
-            if form.data.get('choices', None):
+            if form.data.get('filter_choices', None):
                 response = search.movie(query=search_string, year='2018')
             else:
                 response = search.movie(query=search_string)
 
-            if search.results:
-                context['search_results'] = search.results
+            for result in search.results:
+                movies_choices_list.append((result.get('id'), result.get('title')))
+
+            form = QueryForm(movies_choices_list=movies_choices_list)
+    else:
+        form = QueryForm()
+
+    context = {'form': form}
     
     return render(request, 'tmdb/query_list.html', context)
