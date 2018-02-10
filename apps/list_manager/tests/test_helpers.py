dd8b8730eca2efee25765d7d16a8633a42f01911
Diogo Simão <diogosimao@gmail.com>
2018-02-10 18:21:03 -0200

    Small test

--- apps/list_manager/tests/test_helpers.py
+++ apps/list_manager/tests/test_helpers.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 
 from apps.list_manager import helpers
 
-
 tmdb_movie_search_result_sample = [{'id': 445842,
                                     'title': 'Saber Google',
                                     'poster_path': '/ujkOTrAHLIYE3vTuIIiNZYtyhlV.jpg'}
@@ -28,3 +27,11 @@ class TMDBHelperTest(TestCase):
         output = helpers.retrieve_movies_choices_list_from_tmdb_api_results('string')
         self.assertEqual(output, desired_output)
 
+    @patch('tmdbsimple.Search.movie')
+    def test_retrieve_tmdb_api_results(self, mock_search):
+        pass
+
+    @patch('tmdbsimple.Configuration')
+    def test_retrieve_tmdb_api_config_info(self, mock_config_info):
+        mock_config_info.return_value.info.return_value = tmdb_configuration_info_sample
+        self.assertEqual(helpers.retrieve_tmdb_api_config_info(), tmdb_configuration_info_sample)
